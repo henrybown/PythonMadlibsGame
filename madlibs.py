12dfbad3824d6f6f44027d1a81d7e68948c441cc
Henry Bown <bownerator.mail@gmail.com>
2021-02-01 15:57:15 +0000

    Introduced a random number of bites to eat the main character

--- madlibs.py
+++ madlibs.py
@@ -1,3 +1,5 @@
+import random
+
 # Take user input, and store it in variables
 name = input("Enter a name: ")
 animal1 = input("Enter an animal: ")
@@ -9,11 +11,17 @@ body_part = input("Enter a body part: ")
 # Concatenate the color and 2nd animal to make the 2nd character's name
 animal2 = color + " " + animal2
 
+bites = random.randint(1,10)
+if bites == 1:
+    bites = str(bites) + " bite"
+else:
+    bites = str(bites) + " bites"
+
 # Print out a story, with the variables entered previously
 print(name + " was a " + animal1 + " who lived in " + city)
 print("Unfortunately, " + animal2 + " really like eating " + animal1 + " curry")
 print(name + " was walking to the shop, when " + animal2 + " picked him up with it's " + body_part)
-print("The " + animal1 + " was minced and swallowed in 1 bite, with some ketchup")
+print("The " + animal1 + " was minced and swallowed in " + bites + ", with some ketchup")
 print("Luckily for " + name + ", " + animal2 + " was shot, and " + name + " managed to climb out of its mouth again")
 print("Then " + name + " got hit by a train")
 print("The end")
