7b7310d6abebabd0fa67dd55873bf5f989fb5009
Henry Bown <bownerator.mail@gmail.com>
2021-02-01 15:35:02 +0000

    initial commit

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+name = input("Enter a name: ")
+animal1 = input("Enter an animal: ")
+animal2 = input("Enter an animal: ")
+city = input("Enter a city: ")
+color = input("Enter a color: ")
+body_part = input("Enter a body part: ")
+
+animal2 = color + " " + animal2
+
+print(name + " was a " + animal1 + " who lived in " + city)
+print("Unfortunately, a " + animal2 + " really like eating " + animal1 + " curry")
+print(name + " was walking to the shop, when the " + animal2 + " picked him up with it's " + body_part)
+print("The " + animal1 + " was minced and swallowed in 1 bite, with some ketchup")
+print("Luckily for " + name + ", the " + animal2 + " was shot, and " + name + " managed to climb out of its mouth again")
+print("Then " + name + " got hit by a train")
+print("The end")
